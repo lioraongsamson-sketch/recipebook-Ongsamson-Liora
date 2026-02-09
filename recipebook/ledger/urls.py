9b8b63043279fd14968208a9faedf18c1fbf5554
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-02-09 14:12:02 +0800

    Fixed Recipe links and their paths

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import index, recipes_list, recipe_1, recipe_2
+from .views import recipes_list, recipe_1, recipe_2
 
 app_name = 'ledger'
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('recipes/list/', recipes_list, name="recipes_list"), 
+    path('recipes/list', recipes_list, name="recipes_list"), 
     path('recipe/1', recipe_1, name="recipe_1"),
     path('recipe/2', recipe_2, name="recipe_2"),  
 ]
