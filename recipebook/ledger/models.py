ecd12a1f18c26a1da2ae93fc47180962a0b8400e
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-02-19 12:13:00 +0800

    Fixed issue on reverse function for recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -14,21 +14,26 @@ class Recipe(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return f"{ self.name }"
     
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.id)])
+    
+    class Meta:
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
+
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
-        related_names = 'ingredient_use'
+        related_name = 'recipe'
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_names = 'recipe_ingredient'
+        related_name = 'ingredient'
     )
 # Create your models here.
