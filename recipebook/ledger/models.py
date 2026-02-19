cdd8818ef488f6e9b70e984d2e404ad99ef3b7bd
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-02-19 10:10:15 +0800

    Fixed and Added Absolute Url for Ingredient, Removed the duplicate recipe html file from Lab 1

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,19 +2,22 @@ from django.db import models
 from django.urls import reverse
 
 class Ingredient(models.Model):
-    name = models.CharField(max_legth=100)
-
-    def __str__(self):
-        return self.name
-
-class Recipe(models.Model):
-    name = models.CharField(max_legth=100)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('recipe_detail', args=[str(self.id)])
+        return reverse('ledger:ingredient_detail', args=[str(self.id)])
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ledger:recipe_detail', args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=100)
