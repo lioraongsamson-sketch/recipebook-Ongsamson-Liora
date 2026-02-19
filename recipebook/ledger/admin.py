0d9e3939818e1d5582735453d1692710116c5af2
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-02-19 12:38:29 +0800

    Created an Admin Panel for Recipe

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Recipe, RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+
+admin.site.register(Recipe, RecipeAdmin)
 # Register your models here.
