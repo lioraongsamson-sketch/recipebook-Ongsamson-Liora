e783f6e21635607e9fcefc0fce99b4e0c72b55de
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-02-19 12:25:10 +0800

    Double check list view and detail view from past commits and cleaned code

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -6,7 +6,5 @@ app_name = 'ledger'
 
 urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name="recipes_list"), 
-    ##path('recipe/1', recipe_1, name="recipe_1"),
-    ##path('recipe/2', recipe_2, name="recipe_2"),  
     path('<int:pk>', RecipeDetailView.as_view(), name="recipe_detail")
 ]
