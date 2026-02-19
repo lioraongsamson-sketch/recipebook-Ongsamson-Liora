b7d5f34ab9833a56412114efa4e8d15696ec00b4
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-02-19 12:44:03 +0800

    Created an Inline Admin for Recipe Ingredient

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 from .models import Recipe, RecipeIngredient
 
+class RecipeInLine(admin.TabularInline):
+    model = RecipeIngredient
+
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
+    inlines = [RecipeInLine,]
 
 
 admin.site.register(Recipe, RecipeAdmin)
