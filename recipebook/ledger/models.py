3db2da0d10d9f94926dc2c44030e58d19ee763d1
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-02 08:29:36 +0800

    Created the Profile Model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
@@ -36,4 +37,9 @@ class RecipeIngredient(models.Model):
         on_delete=models.CASCADE,
         related_name = 'ingredients'
     )
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(min_lenght=255,blank=True)
 # Create your models here.
