5c119506685b21edbb54942a411dd3b2e7f1047a
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-02 17:56:29 +0800

    Fixed the min amount for the bio field & fixed the file in PEP8 format

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,41 +1,46 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.TextField(blank=True)
+    bio = models.TextField(
+        validators=[MinLengthValidator(256)]
+    )
 
     def __str__(self):
-        return f"{ self.name }"
+        return f"{self.name}"
+
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{ self.name }"
-    
+        return f"{self.name}"
+
     def get_absolute_url(self):
         return reverse('ledger:ingredient_detail', args=[str(self.id)])
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
-        related_name = 'recipes'
+        related_name='recipes'
     )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
-        return f"{ self.name }"
-    
+        return f"{self.name}"
+
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.id)])
-    
+
     class Meta:
         verbose_name = 'recipe'
         verbose_name_plural = 'recipes'
@@ -46,12 +51,12 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
-        related_name = 'recipe'
+        related_name='recipe'
     )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name = 'ingredients'
+        related_name='ingredients'
     )
 
 # Create your models here.
