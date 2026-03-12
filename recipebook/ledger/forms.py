bf933dd4d5a3d03a48d6c454899c3ffd4033192b
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-12 16:59:39 +0800

    Created Upload Image Page

--- recipebook/ledger/forms.py
+++ recipebook/ledger/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Recipe
+from .models import Recipe, RecipeImage
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = '__all__'
+
+class RecipeImageUploadForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = ['image', 'description']
         
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import RecipeDetailView, RecipeListView, RecipeCreateView
+from .views import RecipeDetailView, RecipeListView, RecipeCreateView, RecipeUploadImageView
 
 app_name = 'ledger'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('recipes/list', RecipeListView.as_view(), name="recipes_list"),
     path('recipe/<int:pk>', RecipeDetailView.as_view(), name="recipe_detail"),
     path('recipe/add', RecipeCreateView.as_view(), name="recipe_create"),
+    path('recipe/<int:pk>/add_image', RecipeUploadImageView.as_view(), name="recipe_upload")
 ]
