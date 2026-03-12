f7639f368a07c9eec2a7882476e5ae72cd6a42b8
lioraongsamson-sketch <liora.ongsamson@student.ateneo.edu>
2026-03-12 08:23:11 +0800

    Added the RecipeImage model to the admin

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from .models import Recipe, RecipeIngredient, Profile
+from .models import Recipe, RecipeIngredient, Profile, RecipeImage
 
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    inlines = [RecipeIngredientInLine,]
+    inlines = [RecipeIngredientInLine, RecipeImageInLine,]
 
 
 class ProfileInLine(admin.StackedInline):
@@ -21,7 +23,6 @@ class ProfileInLine(admin.StackedInline):
 
 class UserAdmin(BaseUserAdmin):
     inlines = [ProfileInLine]
-
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
